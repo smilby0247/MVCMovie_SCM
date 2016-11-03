22a887591f7fc9e19e0e08a077cffe8e84b2d1c0
Shaun Milby <smilby0247@my.sctd.edu>
2016-11-02 20:56:54 -0400

    MvcMovieChanges
    Commit changes.

---
+++ MvcMovie_SCM/MvcMovie_SCM/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcMovie_SCM.Startup))]
+namespace MvcMovie_SCM
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
